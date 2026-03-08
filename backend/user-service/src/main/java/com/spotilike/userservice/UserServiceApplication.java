88d54b83656bf14a1a0d59272ad82506703dd4a6
Krivenyshev Evgeniy <crive.ru@yandex.ru>
2026-03-08 17:58:48 +0700

    fix: 1. add flyway-starter lib      2. fixed jackson and flyway conf

--- backend/user-service/src/main/java/com/spotilike/userservice/UserServiceApplication.java
+++ backend/user-service/src/main/java/com/spotilike/userservice/UserServiceApplication.java
@@ -2,7 +2,7 @@ package com.spotilike.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.spotilike.userservice.model")
