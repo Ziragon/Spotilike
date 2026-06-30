1688cf7bf66228e3754b4d23a547e8f811d0c3e8
Ziragon <crive.ru@yandex.ru>
2026-07-01 00:48:34 +0700

    feature: added bush scripts for init project and generate certs (for linux)

---
+++ backend/init-project.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")" || exit
+
+echo "[1/1] Generating SSL certificates for localhost..."
+bash "scripts/generate-certs.sh"
+
+echo
+echo "=========================================="
+echo "Project initialization complete!"
+echo "=========================================="
+
+read -n 1 -s -r -p "Press any key to continue . . ."
+echo
---
+++ backend/scripts/generate-certs.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Путь относительно корня проекта
+CERT_DIR="./docker/certs"
+
+# Проверка mkcert в системе
+if ! command -v mkcert &> /dev/null; then
+    echo -e "\e[31mError: mkcert not found!\e[0m"
+    exit 1
+fi
+
+mkdir -p "$CERT_DIR"
+
+echo -e "\e[90mChecking local CA...\e[0m"
+mkcert -install
+
+echo -e "\e[32mGenerating certificates for localhost...\e[0m"
+mkcert -cert-file "$CERT_DIR/localhost.pem" \
+       -key-file "$CERT_DIR/localhost-key.pem" \
+       localhost 127.0.0.1 ::1
+
+echo -e "\e[32mDone!\e[0m"
