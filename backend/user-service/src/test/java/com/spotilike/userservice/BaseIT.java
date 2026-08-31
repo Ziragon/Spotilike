d0df12c99f7be1e4235aa676df3468607a41524e
Ziragon <crive.ru@yandex.ru>
2026-08-31 16:11:02 +0700

    test(user-service):     1. added testcontainers lib to auto configure containers     2. removed CatalogServiceApplicationTests.java

--- backend/user-service/src/test/java/com/spotilike/userservice/BaseIT.java
+++ backend/user-service/src/test/java/com/spotilike/userservice/BaseIT.java
@@ -5,26 +5,13 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @ActiveProfiles("test")
 public abstract class BaseIT {
 
-    @SuppressWarnings("resource")
+    @ServiceConnection
     static final PostgreSQLContainer<?> postgres =
-            new PostgreSQLContainer<>("postgres:16")
-                    .withDatabaseName("test_db")
-                    .withUsername("test")
-                    .withPassword("test");
-
-    static {
-        postgres.start();
-    }
-
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.password", postgres::getPassword);
-    }
+            new PostgreSQLContainer<>("postgres:17");
 }
